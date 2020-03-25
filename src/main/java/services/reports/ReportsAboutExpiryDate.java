bd1fb38d2edb893a1a0c2079904744c9992011d9
Kornyshev <kornyshev.e@yandex.ru>
2020-03-25 22:39:02 +0300

    25.03.2020 - Half of business services are ready

---
+++ src/main/java/services/reports/ReportsAboutExpiryDate.java
@@ -0,0 +1,68 @@
+package services.reports;
+
+import dao.impl.CreditCardDAOImpl;
+import services.entities.CreditCard;
+
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ReportsAboutExpiryDate {
+
+    public List<CreditCard> getSoonExpiredCards(int months) {
+        List<CreditCard> cards = new ArrayList<>();
+        try {
+            cards = new CreditCardDAOImpl().retrieveAllCards();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return cards.
+                stream().
+                filter(card -> comparingDates(card.expiryDate, currentDate()) > months).
+                collect(Collectors.toList());
+    }
+
+    public List<CreditCard> getAlreadyExpiredCards() {
+        List<CreditCard> cards = new ArrayList<>();
+        try {
+            cards = new CreditCardDAOImpl().retrieveAllCards();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return cards.
+                stream().
+                filter(card -> comparingDates(card.expiryDate, currentDate()) == -1).
+                collect(Collectors.toList());
+    }
+
+    private static String currentDate() {
+        Calendar calendar = new GregorianCalendar();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        if (month < 10) {
+            return "0" + month + "/" + year;
+        } else {
+            return "" + month + "/" + year;
+        }
+    }
+
+    private static int comparingDates(String dateOnCard, String current) {
+        int[] curr = new int[]
+                {Integer.parseInt(current.split("/")[0]), Integer.parseInt(current.split("/")[1])};
+        int[] onCard = new int[]
+                {Integer.parseInt(dateOnCard.split("/")[0]), Integer.parseInt(dateOnCard.split("/")[1])};
+        if (onCard[1] < curr[1] ||
+                (onCard[1] == curr[1] &&
+                        onCard[0] < curr[0])) {
+            return -1;
+        }
+        if (onCard[1] == curr[1]) {
+            return onCard[0] - curr[0];
+        } else {
+            return
+                    (onCard[1] - curr[1] - 1) * 12 +
+                            (12 - curr[0]) +
+                            onCard[0];
+        }
+    }
+}
